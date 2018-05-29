bca06bb4bf3489f0b070097cbcab35bc644e90cb
Joe Lv <joel@wicresoft.com>
2018-05-29 10:38:29 +0800

    first commit

---
+++ App46/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using System;
+
+namespace App46
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            LatLng latlng = new LatLng(Convert.ToDouble(13.0291), Convert.ToDouble(10.2083));
+            CameraUpdate camera = CameraUpdateFactory.NewLatLngZoom(latlng, 15);
+            googleMap.MoveCamera(camera);
+            MarkerOptions options = new MarkerOptions().SetPosition(latlng).SetTitle("Chennai");
+            googleMap.AddMarker(options);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            SupportMapFragment mapFragment = (SupportMapFragment)SupportFragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
+        }
+    }
+}
+
